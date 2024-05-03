09d2d8b40bcccaf80892d3d3e03c483bf3764248
gogoi2k20 <gogoi2k20@gmail.com>
2024-05-03 13:05:51 +0530

    Update Linting

--- app/app/calc.py
+++ app/app/calc.py
@@ -6,5 +6,5 @@ Calculator Functions
 
 def add(x, y):
     # Add x and y and return result
-    
+   
     return x + y
--- app/app/tests.py
+++ app/app/tests.py
@@ -9,8 +9,8 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     "Test the calc Module"
-  
+
     def test_add_numbers(self):
         "Test adding numbers together"
-        res=calc.add(5, 6)  
+        res = calc.add(5, 6)  
         self.assertEqual(res, 11)
